80d03e5e489d0b0eb2d17a7fa6b5c859b99776d2
GunnySensei <gunnerwagoner@hotmail.com>
2022-03-20 11:13:56 -0500

    initial structure and User model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+..+/, "Not an email"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce(
+    (total, comment) => totalt + comment.replies.length + 1,
+    0
+  );
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
